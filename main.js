cc4a2421ebeb4b20abbbaf0cfdf0179086c123af
NurayymJeenbekova <nurayym.jeenbekova.kk@gmail.com>
2022-01-28 23:45:18 +0600

    #3

--- main.js
+++ main.js
@@ -5,3 +5,7 @@ document.write(first.replace(/@/g, '!') + "<br>");
 //2
 const second = 'js';
 document.write(second.toUpperCase() +"<br>");
+
+//3
+const third = 'JS';
+document.write(third.toLowerCase() + "<br>");
